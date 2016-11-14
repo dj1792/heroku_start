ddfe7c54844acd614a1a43f0d2597c898f01480a
dj1792 <djayaswa@andrew.cmu.edu>
2016-11-14 01:41:29 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+gem 'sinatra', require: 'sinatra/base'
+gem 'json'
+gem 'activesupport'
+gem "rake"
+gem 'activerecord'
+gem 'sinatra-activerecord' 
+gem 'sqlite3'
+gem 'sinatra-contrib',require: false
+gem 'haml'
+gem 'builder'
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require "active_support/all"
+require 'sinatra/activerecord'
+require 'json'
+
+#before do
+#	content_type :json
+#end
+
+enable :sessions
+
+get'/' do
+	error 401
+end
+
+error 401 do
+	{ error: "Not allowed"}.to_json
+end
+
